6997268bbfa0cb23b07ce0009805424e6e86ed7e
juditvarro <dr.judit.varro@gmail.com>
2025-08-02 15:29:05 +0200

    feat: update form to collect and calculate resutls

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { CalculatorService } from "../calculator.service";
 
 @Component({
-  selector: 'app-input',
+  selector: "app-input",
   standalone: true,
-  imports: [],
-  templateUrl: './input.component.html',
-  styleUrl: './input.component.css'
+  imports: [FormsModule],
+  templateUrl: "./input.component.html",
+  styleUrl: "./input.component.css",
 })
 export class InputComponent {
+  private calculatorService = inject(CalculatorService);
 
+  initialInvestmentInput = 0;
+  annualInvestmentInput = 0;
+  returnInput = 0;
+  durationInput = 0;
+
+  onSubmit() {
+    this.calculatorService.calculateInvestmentResults(
+      this.initialInvestmentInput,
+      this.annualInvestmentInput,
+      this.returnInput,
+      this.durationInput
+    );
+
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.initialInvestmentInput = 0;
+    this.annualInvestmentInput = 0;
+    this.returnInput = 0;
+    this.durationInput = 0;
+  }
 }
